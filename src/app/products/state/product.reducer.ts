67672627657375cbf8f9ecfbc9bfd1359ce6d97a
Patricia Silva <Patricia.Silva@ctw.bmwgroup.com>
2020-02-03 18:43:23 +0000

    Strongly Typing the State and Building Selectors

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -9,14 +9,14 @@ export interface State extends fromRoot.State {
 
 export interface ProductState {
     showProductCode: boolean;
-    currentProduct: Product;
+    currentProductId: number | null;
     products: Product[];
     error: string;
 }
 
 const initialState: ProductState = {
     showProductCode: true,
-    currentProduct: null,
+    currentProductId: null,
     products: [],
     error: ''
 };
@@ -28,9 +28,27 @@ export const getShowProductCode = createSelector(
     state => state.showProductCode
 );
 
+export const getCurrentProductId = createSelector(
+    getProductFeatureState,
+    state => state.currentProductId
+);
+
 export const getCurrentProduct = createSelector(
     getProductFeatureState,
-    state => state.currentProduct
+    getCurrentProductId,
+    (state, currentProductId) => {
+        if (currentProductId === 0) { // new product - no id defined
+            return {
+                id: 0,
+                productName: '',
+                productCode: 'New',
+                description: '',
+                starRating: 0
+            };
+        } else { // if the product id exists, return the product that matches; returns null if no product is currently selected
+            return currentProductId ? state.products.find(p => p.id === currentProductId) : null;
+        }
+    }
 );
 
 export const getProducts = createSelector(
@@ -55,25 +73,19 @@ export function reducer(state = initialState, action: ProductActions): ProductSt
         case ProductActionTypes.SetCurrentProduct:
             return {
                 ...state,
-                currentProduct: { ...action.payload }
+                currentProductId: action.payload.id
             };
 
         case ProductActionTypes.ClearCurrentProduct:
             return {
                 ...state,
-                currentProduct: null
+                currentProductId: null
             };
 
         case ProductActionTypes.InitializeCurrentProduct:
             return {
                 ...state,
-                currentProduct: {
-                    id: 0,
-                    productName: '',
-                    productCode: 'New',
-                    description: '',
-                    starRating: 0
-                }
+                currentProductId: 0
             };
 
         case ProductActionTypes.LoadSuccess:

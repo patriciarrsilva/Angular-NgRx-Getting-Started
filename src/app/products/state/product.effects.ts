dec9ecee2f89210523629b7a926a1b33c5fef76a
Patricia Silva <Patricia.Silva@ctw.bmwgroup.com>
2020-02-04 10:25:16 +0000

    Building the Effect

--- src/app/products/state/product.effects.ts
+++ src/app/products/state/product.effects.ts
@@ -4,7 +4,8 @@ import { ProductService } from '../product.service';
 import * as productActions from './product.actions';
 import { mergeMap, map, catchError } from 'rxjs/operators';
 import { Product } from '../product';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class ProductEffects {
@@ -12,11 +13,21 @@ export class ProductEffects {
                 private productService: ProductService) { }
 
     @Effect()
-    loadProducts$ = this.actions$.pipe(
+    loadProducts$: Observable<Action> = this.actions$.pipe(
         ofType(productActions.ProductActionTypes.Load),
         mergeMap((action: productActions.Load) => this.productService.getProducts().pipe(
             map((products: Product[]) => (new productActions.LoadSuccess(products))),
             catchError(err => of(new productActions.LoadFail(err)))
+        ))
+    );
+
+    @Effect()
+    updateProduct$: Observable<Action> = this.actions$.pipe(
+        ofType(productActions.ProductActionTypes.UpdateProduct),
+        map((action: productActions.UpdateProduct) => action.payload),
+        mergeMap((product: Product) => this.productService.updateProduct(product).pipe(
+            map(updatedProduct => (new productActions.UpdateProductSuccess(updatedProduct))),
+            catchError(err => of(new productActions.UpdateProductFail(err)))
         ))
     );
 }
